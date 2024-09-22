3bd98330649e142c77dbef4daf6553c79737885e
shukri1236 <shukri1236>
2024-09-22 13:10:48 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,38 @@
+
 #!/usr/bin/env python3
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
+happy_new_year()    
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_list = [x ** 2 for x in int_list]
+    return square_list
+
+result = square_integers([1, 2, 3, 4, 5])
+print(result)
+
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for number in range(1, 101): 
+        if number % 3 == 0 and number % 5 == 0:
+           print("FizzBuzz")
+        elif number % 3 == 0:
+           print("Fizz")
+        elif number % 5 == 0:
+           print("Buzz")
+        else:
+           print(number)
+
+fizzbuzz()
+    
+    
